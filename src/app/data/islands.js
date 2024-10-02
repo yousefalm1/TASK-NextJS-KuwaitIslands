35b033c8a3128a926851324133c5cab7785682af
Octowl <alsaff1987@gmail.com>
2024-10-02 13:37:21 +0300

    add data file

---
+++ src/app/data/islands.js
@@ -0,0 +1,52 @@
+const islands = [
+  {
+    id: 1,
+    name: "Failaka",
+    img: "https://www.gckuwait.com/wp-content/uploads/2017/01/gulf-consult-Failaka-Island-Master-Plan1-1.jpg",
+    visitors: 0,
+  },
+  {
+    id: 2,
+    name: "Boubyan",
+    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg",
+    visitors: 0,
+  },
+  {
+    id: 3,
+    name: "Warbah",
+    img: "https://i.pinimg.com/originals/84/3c/9b/843c9b836c82c8010d84195f72cd6f66.jpg",
+    visitors: 0,
+  },
+  {
+    id: 4,
+    name: "Umm Al Namil",
+    img: "https://static.trip101.com/main_pics/207924/medium.jpg",
+    visitors: 0,
+  },
+  {
+    id: 5,
+    name: "Ouha",
+    img: "https://content.fortune.com/wp-content/uploads/2017/09/blk09-15_a.jpg",
+    visitors: 0,
+  },
+  {
+    id: 6,
+    name: "Kubbar",
+    img: "https://kuwaitlocal.com/js/kcfinder/upload/images/Boat%20Trip%20To%20Kubbar%20Island9.jpg",
+    visitors: 0,
+  },
+  {
+    name: "Umm Al Maradim",
+    id: 7,
+    img: "https://www.kuna.net.kw/NewsPictures/2016/8/11/a95bfb4e-9189-41d7-9879-af4f9737aa81.jpg",
+    visitors: 0,
+  },
+  {
+    id: 8,
+    name: "Qaruh",
+    img: "https://news.kuwaittimes.net/website/wp-content/uploads/2021/06/local8.jpg",
+    visitors: 0,
+  },
+];
+
+export default islands;
