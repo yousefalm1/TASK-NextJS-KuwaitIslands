b53d17bf3c4e40b953bcd0fae7662cfcd80a6d4e
Octowl <alsaff1987@gmail.com>
2024-10-02 13:56:24 +0300

    configure next to accept images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,41 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.gckuwait.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'photos.wikimapia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.trip101.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'content.fortune.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kuwaitlocal.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.kuna.net.kw',
+      },
+      {
+        protocol: 'https',
+        hostname: 'news.kuwaittimes.net',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
